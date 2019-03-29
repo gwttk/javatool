3b8e2ee305f8e78fdd7251ea1544bab97d6267eb
Immueggpain <Immueggpain@users.noreply.github.com>
2019-03-29 23:45:29 +0800

    Launcher

---
+++ random-server/src/main/java/com/github/immueggpain/randomserver/RandomServer.java
@@ -0,0 +1,12 @@
+package com.github.immueggpain.randomserver;
+
+import com.github.immueggpain.randomserver.Launcher.ServerSettings;
+
+public class RandomServer {
+
+	public void run(ServerSettings settings) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
