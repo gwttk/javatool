18680645fe6e1cdb8bef6dbd89f5f126a14b96f8
Immueggpain <Immueggpain@users.noreply.github.com>
2019-06-15 13:55:22 +0800

    shellexecute

--- src/main/java/com/github/immueggpain/javatool/Launcher.java
+++ src/main/java/com/github/immueggpain/javatool/Launcher.java
@@ -6,10 +6,9 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
 
-@Command(description = "Prints the checksum (MD5 by default) of a file to STDOUT.", name = "javatool",
-		mixinStandardHelpOptions = true, version = Launcher.VERSTR,
-		subcommands = { HelpCommand.class, Hash.class, RandomServer.class, ConnInfoServer.class, ConnInfoClient.class,
-				Chatter.class, UdpSend.class, UdpRecv.class })
+@Command(description = "Prints the checksum (MD5 by default) of a file to STDOUT.", name = "javatool", mixinStandardHelpOptions = true, version = Launcher.VERSTR, subcommands = {
+		HelpCommand.class, Hash.class, RandomServer.class, ConnInfoServer.class, ConnInfoClient.class, Chatter.class,
+		UdpSend.class, UdpRecv.class, ShellExecute.class })
 public class Launcher implements Callable<Void> {
 
 	public static final String VERSTR = "1.0.0";
---
+++ src/main/java/com/github/immueggpain/javatool/ShellExecute.java
@@ -0,0 +1,34 @@
+package com.github.immueggpain.javatool;
+
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+import com.sun.jna.platform.win32.Shell32;
+import com.sun.jna.platform.win32.WinDef.INT_PTR;
+import com.sun.jna.platform.win32.WinUser;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(description = "ShellExecute open a file on windows", name = "shellexec", mixinStandardHelpOptions = true, version = Launcher.VERSTR)
+public class ShellExecute implements Callable<Void> {
+
+	@Option(names = { "-f", "--file" }, required = true, description = "file to open")
+	public Path file;
+
+	@Override
+	public Void call() throws Exception {
+		System.out.println("opening " + file);
+		System.out.println(Shell32.INSTANCE);
+		INT_PTR r = Shell32.INSTANCE.ShellExecute(null, null, file.toString(), null, file.getParent().toString(),
+				WinUser.SW_SHOWNORMAL);
+		System.out.println(r);
+		if (r.longValue() == 31) {
+			r = Shell32.INSTANCE.ShellExecute(null, "openas", file.toString(), null, file.getParent().toString(),
+					WinUser.SW_SHOWNORMAL);
+		}
+		System.out.println(r);
+		return null;
+	}
+
+}
