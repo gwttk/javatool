c2183e57724c39c1dfa9a32dfec7846d5f390643
Immueggpain <Immueggpain@users.noreply.github.com>
2019-07-10 17:30:50 +0800

    1.2.0

--- src/main/java/com/github/immueggpain/javatool/Launcher.java
+++ src/main/java/com/github/immueggpain/javatool/Launcher.java
@@ -6,14 +6,14 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
 
-@Command(description = "Prints the checksum (MD5 by default) of a file to STDOUT.", name = "javatool",
-		mixinStandardHelpOptions = true, version = Launcher.VERSTR,
+@Command(description = "Toolkit written in java.", name = "javatool", mixinStandardHelpOptions = true,
+		version = Launcher.VERSTR,
 		subcommands = { HelpCommand.class, Hash.class, RandomServer.class, ConnInfoServer.class, ConnInfoClient.class,
 				Chatter.class, UdpSend.class, UdpRecv.class, ShellExecute.class, SpeedTestClient.class,
 				SpeedTestServer.class, Base64.class, HttpPost.class, HttpSend.class })
 public class Launcher implements Callable<Void> {
 
-	public static final String VERSTR = "1.0.0";
+	public static final String VERSTR = "1.2.0";
 
 	public static void main(String[] args) {
 		new CommandLine(new Launcher()).setCaseInsensitiveEnumValuesAllowed(true).execute(args);
