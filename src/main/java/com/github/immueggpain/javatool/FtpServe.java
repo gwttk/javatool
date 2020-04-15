bbb22f32f17bca7e4b9a9157268fee08fda1848e
Immueggpain <Immueggpain@users.noreply.github.com>
2020-04-15 18:40:04 +0800

    Update FtpServe.java

--- src/main/java/com/github/immueggpain/javatool/FtpServe.java
+++ src/main/java/com/github/immueggpain/javatool/FtpServe.java
@@ -1,12 +1,16 @@
 package com.github.immueggpain.javatool;
 
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authority;
 import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
+
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
@@ -37,6 +41,9 @@ public class FtpServe implements Callable<Void> {
 		BaseUser user = new BaseUser();
 		user.setName("anonymous");
 		user.setHomeDirectory(homeDir);
+		ArrayList<Authority> authorities = new ArrayList<>();
+		authorities.add(new WritePermission());
+		user.setAuthorities(authorities);
 		serverFactory.getUserManager().save(user);
 		// start the server
 		FtpServer server = serverFactory.createServer();
