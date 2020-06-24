964050e21349bd4cbfebd956e9a60aa2f17a5f1f
Immueggpain <Immueggpain@users.noreply.github.com>
2020-06-24 08:23:52 +0800

    Update MouseMove.java

--- src/main/java/com/github/immueggpain/javatool/MouseMove.java
+++ src/main/java/com/github/immueggpain/javatool/MouseMove.java
@@ -1,7 +1,16 @@
 package com.github.immueggpain.javatool;
 
-import java.awt.Robot;
+import java.awt.MouseInfo;
 import java.util.concurrent.Callable;
+
+import com.sun.jna.platform.win32.BaseTSD.ULONG_PTR;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinDef.DWORD;
+import com.sun.jna.platform.win32.WinDef.LONG;
+import com.sun.jna.platform.win32.WinUser;
+import com.sun.jna.platform.win32.WinUser.INPUT;
+
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
@@ -10,7 +19,7 @@ import picocli.CommandLine.Spec;
 @Command(description = "Move mouse ", name = "mmove", mixinStandardHelpOptions = true, version = Launcher.VERSTR)
 public class MouseMove implements Callable<Void> {
 
-	@Option(names = { "-p", "--server-port" }, required = true, description = "server listening port")
+	@Option(names = { "-x", "--dx" }, required = true, description = "mouse move dx")
 	public int serverPort;
 
 	@Spec
@@ -18,8 +27,21 @@ public class MouseMove implements Callable<Void> {
 
 	@Override
 	public Void call() throws Exception {
-		Robot robot = new Robot();
-		robot.mouseMove(0, 0);
+		System.out.println(MouseInfo.getPointerInfo().getLocation());
+
+		WinUser.INPUT input = new WinUser.INPUT();
+		input.type = new WinDef.DWORD(WinUser.INPUT.INPUT_MOUSE);
+		input.input.setType("mi");
+		input.input.mi.dx = new LONG(200);
+		input.input.mi.dy = new LONG(0);
+		input.input.mi.mouseData = new DWORD(0);
+		input.input.mi.dwFlags = new DWORD(1);
+		input.input.mi.time = new DWORD(0);
+		input.input.mi.dwExtraInfo = new ULONG_PTR(0);
+
+		User32.INSTANCE.SendInput(new DWORD(1), new INPUT[] { input }, input.size());
+
+		System.out.println(MouseInfo.getPointerInfo().getLocation());
 		return null;
 	}
 
